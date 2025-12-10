cd64893b029ba7a30e6365b339afe966f951d99c
Eren <jellyslogoman12345@gmail.com>
2025-12-10 15:27:22 +0100

    Use correct Firebase env var

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "egyai-89547.firebaseapp.com",
@@ -13,11 +12,9 @@ const firebaseConfig = {
   measurementId: "G-GN2NLT0423",
 };
 
-// Prevent duplicate initialization in Next.js
 export const firebaseApp =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-// Load analytics only in browser + only if supported
 export const analytics =
   typeof window !== "undefined"
     ? await isSupported().then((supported) =>
